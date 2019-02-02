73e89b4bc46298c5e83d19688b61208377eeef1c
cindyfeldman <cindyfeldman>
2019-02-02 12:07:12 -0800

    printed name to textField

--- app/src/main/java/com/example/extstudent/assignment1/MainActivity.java
+++ app/src/main/java/com/example/extstudent/assignment1/MainActivity.java
@@ -13,6 +13,7 @@ private TextView lastNameTextField = null;
 private String firstName = "" ;
 private String lastName = "";
 private Button SubmitButton = null;
+private TextView resultTextView = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +22,18 @@ private Button SubmitButton = null;
         //initialize variables
         this.firstNameTextField = findViewById(R.id.editTextFirstName);
         this.lastNameTextField = findViewById(R.id.editTextLastName);
+        this.resultTextView = findViewById(R.id.textViewResult);
         this.SubmitButton = findViewById(R.id.button);
+
         this.SubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
              Log.d( "My Submit button",  "OnClick pressed");
              setUserInfo();
+             showResult();
              Log.d("First Name: ", firstName);
              Log.d("Last Name: ", lastName);
+
             }
         });
 
@@ -39,5 +44,10 @@ private Button SubmitButton = null;
         this.firstName = this.firstNameTextField.getText().toString();
         this.lastName = this.lastNameTextField.getText().toString();
     }
+    private void showResult() {
+        String resultStr = "Hi, nice to meet you " + firstName + " " + lastName;
+        this.resultTextView.setText(resultStr);
+    }
+
 
 }
